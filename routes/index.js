f379eb0ccad9009ee74329ffe1f7bfef4066c8a6
Moritz Messner <48797914+MoritzMessner@users.noreply.github.com>
2021-08-03 16:27:14 +0200

    call it a day

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,12 @@ const name = "Moritz Messner";
 const title = "Portfolio Website";
 const social = { "github": "https://github.com/MoritzMessner", "twitter": "https://twitter.com/moe_dev" };
 const profile_image = "/images/Moritz Messner.jpg";
-
+const video_background = "/images/drone_trees.mp4"
+    //let projects = require('/images/projects.json');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: title, name: name, links: social, profile_image: profile_image });
+router.get('/', function(req, res, next) {
+    res.render('index', { title: title, name: name, links: social, profile_image: profile_image, video: video_background });
 });
 
 module.exports = router;
