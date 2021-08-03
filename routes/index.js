091a0c41b032db981ccb156c3fd7f73743425ee9
Moritz Messner <48797914+MoritzMessner@users.noreply.github.com>
2021-08-03 15:38:04 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const name = "Moritz Messner";
+const title = "Portfolio Website";
+const social = { "github": "https://github.com/MoritzMessner", "twitter": "https://twitter.com/moe_dev" };
+const profile_image = "/images/Moritz Messner.jpg";
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moritz Portfolio' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: title, name: name, links: social, profile_image: profile_image });
 });
 
 module.exports = router;
